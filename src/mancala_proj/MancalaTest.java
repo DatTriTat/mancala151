26dda055f085abff1d1500184f1f6079e2e6f151
Sandra Le <lesandra007@gmail.com>
2023-04-11 22:19:42 -0700

    ...

---
+++ src/mancala_proj/MancalaTest.java
@@ -0,0 +1,11 @@
+package mancala_proj;
+/**
+ * This program tests the Mancala game
+ * @author pebbles
+ * @version 4/11/2023
+ */
+public class MancalaTest {
+	public static void main(String[] args) {
+		
+	}
+}
